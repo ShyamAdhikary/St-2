62da1e3793b41286f5cc480483abbacc61720c17
ShyamAdhikary <58951101+ShyamAdhikary@users.noreply.github.com>
2022-02-12 20:25:07 +0530

    Add files via upload

---
+++ St-Test-4.py
@@ -0,0 +1,30 @@
+# 
+# Env - saenv5
+# Import required modules
+#
+import os
+import sys
+#import cv2
+import numpy as np
+#import matplotlib
+#from matplotlib import pyplot as plt
+from datetime import datetime
+import streamlit as st
+from PIL import Image
+#
+#st.write ("Testing starts-1:", datetime.today().strftime("%x %H:%M:%S"))
+
+#print version no
+st.write ('Python     : {}'.format(sys.version))
+st.write ('Numpy      : {}'.format(np.__version__))
+#st.write ('Matplotlib : {}'.format(matplotlib.__version__))
+#st.write ('OpenCV     : {}'.format(cv2.__version__))
+
+# 
+st.title(" -- Streamlit testing -- ")
+
+#
+st.title(" -- Streamlit testing example for mobile -- ")
+st.write ('Example ends here:', datetime.today().strftime("%x %H:%M:%S"))
+st.title(" -- Streamlit testing Ends -- ")
+# 
